8c6c846641aebb89f4b9ac885b0235a3a31a9fad
“an_2750” <nslogyanmingyue@126.com>
2015-09-20 21:36:03 +0800

    新的工程，用来测试github的提交

--- NewProject_9.20/ViewController.m
+++ NewProject_9.20/ViewController.m
@@ -16,8 +16,8 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-}
+    NSLog(@"用来测试github的使用__________________9.20");
+    }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
