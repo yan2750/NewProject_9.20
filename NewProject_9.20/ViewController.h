416effe455c7c50a360efcbf2cb9e23dcf200baa
“an_2750” <nslogyanmingyue@126.com>
2015-09-20 21:34:51 +0800

    Initial Commit

---
+++ NewProject_9.20/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NewProject_9.20
+//
+//  Created by 闫名月 on 15/9/20.
+//  Copyright (c) 2015年 闫名月. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ NewProject_9.20/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  NewProject_9.20
+//
+//  Created by 闫名月 on 15/9/20.
+//  Copyright (c) 2015年 闫名月. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
