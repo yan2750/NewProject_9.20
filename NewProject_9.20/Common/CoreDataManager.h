af180b4bfb75daa91f03e88d105e3d0e5107b6fb
ymy <nslogyanmingyue@126.com>
2015-12-03 18:13:17 +0800

    CoreData管理器
    CoreData的添加和删除操作在这里做操作。

---
+++ NewProject_9.20/Common/CoreDataManager.h
@@ -0,0 +1,15 @@
+//
+//  CoreDataManager.h
+//  NewProject_9.20
+//
+//  Created by YanMingyue on 15/12/3.
+//  Copyright © 2015年 闫名月. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CoreDataManager : NSObject
+
+- (void)commonInit;
+
+@end
---
+++ NewProject_9.20/Common/CoreDataManager.m
@@ -0,0 +1,53 @@
+//
+//  CoreDataManager.m
+//  NewProject_9.20
+//
+//  Created by YanMingyue on 15/12/3.
+//  Copyright © 2015年 闫名月. All rights reserved.
+//
+
+#import "CoreDataManager.h"
+#import "AppDelegate.h"
+
+@interface CoreDataManager()
+
+@property (nonatomic, assign) AppDelegate *appDelegate;
+
+@end
+
+@implementation CoreDataManager
+
++ (instancetype)shareInstace {
+    static CoreDataManager *coreDataManager = nil;
+    static dispatch_once_t once_t;
+    dispatch_once(&once_t, ^{
+        coreDataManager = [[self alloc] init];
+    });
+    return coreDataManager;
+}
+
+- (id)init {
+    if (self = [super init]) {
+        [self commonInit];
+    }
+    return self;
+}
+
+- (void)commonInit {
+    self.appDelegate = (AppDelegate *)[[UIApplication sharedApplication]delegate];
+}
+
+-(IBAction)save:(UIButton *)sender
+{
+    NSManagedObject *object = [NSEntityDescription insertNewObjectForEntityForName:@"Province" inManagedObjectContext:self.appDelegate.managedObjectContext];
+    
+    [object setValue:<#(nullable id)#> forKey:<#(nonnull NSString *)#>]
+    /*
+    [object setValue:citytext.text forKey:@"cityname"];
+    [object setValue:provincetext.text forKey:@"provincename"];
+    
+    [self.mydelegate saveContext];
+     */
+    
+    
+}@end
